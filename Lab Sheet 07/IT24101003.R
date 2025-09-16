0dd167535dfbaaad32c79882c408628c8d47d16a
Nuwantha Pasindu <it24101003@my.sliit.lk>
2025-09-16 00:14:16 -0700

    Add files via upload

---
+++ Lab Sheet 07/IT24101003.R
@@ -0,0 +1,14 @@
+setwd("C:/Users/Nuwantha/Desktop/IT24101003")
+#Q1
+punif(25,max=40, lower.tail = TRUE) - punif(10,max = 40, lower.tail = TRUE)
+
+#Q2
+pexp(2, rate = 0.33, lower.tail = TRUE)
+
+#Q3
+#1
+pnorm(130, mean = 100, sd=15, lower.tail = TRUE)
+
+#2
+qnorm(0.05, mean = 100, sd=15, lower.tail = FALSE)
+
